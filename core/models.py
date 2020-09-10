332a262f7f3dbde36f392cb6bf906141ed5d9cd1
rafaelsom <rafaelsomr1@gmail.com>
2020-09-10 00:17:03 -0300

    Criado tabela de eventos para trabalhar com a agenda

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+#criando uma tabela....vamos chamá-la de eventos -> com os campos:
+#TÍTULO - DESCRIÇÃO - LOCAL DO EVENTO - DATA DO EVENTO - DATA CRIAÇÃO
+
+class Evento(models.Model):                                             #criando a CLASSE evento
+    titulo = models.CharField(max_length=100)                           #TÍTULO -> campo; CHARFIELD -> tipo do campo; MAX_LENGTH (limite de caracteres que ele vai ter) -> parâmetro do campo
+    descricao = models.TextField(blank=True, null=True)                 #igual -> pode ser em branco e nulo
+    data_evento = models.DateTimeField(verbose_name='Data do evento')   #DATETIMEFIELD -> campo de data e hora; não pode ser nulo
+    data_criacao = models.DateTimeField(auto_now=True)                  #igual...pra saber a data que o usuário inseriu esse evento no meu banco; esse 'dado' tem que ser automático...parametro AUTO_NOW
+#essa tabela é do usuário...então bora criar o usuario
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#MIGRAR ESSA CLASSE PARA UMA TABELA MESMO; ESSA TABELA PARA MEU BANCO DE DADOS
+#NO TERMINAL => PYTHON MANAGE.PY MAKEMIGRATIONS CORE; antes instalar em settings; acrecentar core em INSTALLED_APPS
+
+#para minha tabela pura...do jeito que tá...sem que o progrma auto-arrume -> exigir que a tabela chame evento
+
+    class Meta:
+        db_table = 'evento'
+
+#tratando o objeto
+#usando a função STR
+
+    def __str__(self):
+        return self.titulo
