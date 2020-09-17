e9b49dc62b3d6b51f46da1253b99a3fa3a618969
rafaelsom <rafaelsomr1@gmail.com>
2020-09-17 17:39:59 -0300

    Incluído template para inserção de eventos da agenda. Incluído o gitignore para ignorar os arquivos compilados

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,6 +21,9 @@ from django.views.generic import  RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
+#criando nova rota para inserção de dados
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
     #path('', views.index)
     path('', RedirectView.as_view(url='/agenda/')),
     path('login/', views.login_user),
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from core.models import Evento      #imortat core.models -> para listar toddos eventos cadastrados
+from core.models import Evento      #importar core.models -> para listar toddos eventos cadastrados
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -37,3 +37,21 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+#inserção de dados
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('título')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
